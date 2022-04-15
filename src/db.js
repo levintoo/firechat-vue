db26e47308ccd6d8868acabf97aa254ec4e2c02c
levintoo <tooklevin@gmail.com>
2022-04-15 14:49:33 +0300

    login ui

--- src/db.js
+++ src/db.js
@@ -1,6 +1,14 @@
-import firebase from "firebase/app";
-import "firebase/database";
+import firebase from 'firebase/compat/app';
+import "firebase/compat/database";
 
 const config = {
-    
+    apiKey: "AIzaSyApYlmx2zU90khhCP-gtq__kNhCX353YgU",
+    authDomain: "firechat-vue-4d879.firebaseapp.com",
+    projectId: "firechat-vue-4d879",
+    storageBucket: "firechat-vue-4d879.appspot.com",
+    messagingSenderId: "444614631981",
+    appId: "1:444614631981:web:69579347555868b2fa8ea4"
 }
+
+const db = firebase.initializeApp(config);
+export default db;
